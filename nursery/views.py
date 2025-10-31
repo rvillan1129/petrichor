311ac445cb6766aa004e21a2e244da1a6ca1cea4
Rigo Villanueva <rvillanueva2@philasd.org>
2025-10-31 16:58:52 -0400

    add index view and base_generic/index templates

--- nursery/views.py
+++ nursery/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from .models import Plant, PlantInstance, Location, CommonName
 
-# Create your views here.
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_plants = Plant.objects.all().count()
+    num_instances = PlantInstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_purchased = PlantInstance.objects.filter(status__exact='p').count()
+
+    # The 'all()' is implied by default.
+    num_locations = Location.objects.count()
+
+    context = {
+        'num_plants': num_plants,
+        'num_instances': num_instances,
+        'num_instances_purchased': num_instances_purchased,
+        'num_locations': num_locations,
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
